b8e3ab4dc472cba9785a3a66a4bd8ef99c46482d
slave in a new era <33710058+bach001@users.noreply.github.com>
2022-10-11 02:51:25 +0800

    Update amixer_toggle.sh

--- amixer_toggle.sh
+++ amixer_toggle.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# for details https://askubuntu.com/questions/65764/how-do-i-toggle-sound-with-amixer?_gl=1*e645kx*_ga*NDE5NTA1NDA0LjE2NjUwMjQ2MzE.*_ga_S812YQPLT2*MTY2NTQyNzY2Ni4zLjAuMTY2NTQyNzY2Ni4wLjAuMA..#286437
+# https://superuser.com/questions/805525/why-is-unmute-not-working-with-amixer-command
+
 CURRENT_STATE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
 
 if [[ $CURRENT_STATE == '[on]' ]]; then
